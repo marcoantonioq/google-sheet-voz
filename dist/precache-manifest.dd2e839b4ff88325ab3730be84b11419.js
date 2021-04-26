d013bf09be33fa8802eda06361a2c5b33ecc98ba
Marco Antônio Queiroz <marco.aq7@gmail.com>
2021-04-26 19:17:22 -0300

    New build

---
+++ dist/precache-manifest.dd2e839b4ff88325ab3730be84b11419.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cdfbbefe3d70ff02630",
+    "url": "/css/app.css"
+  },
+  {
+    "revision": "a46a4b91beb359609890498bd1637663",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8cdfbbefe3d70ff02630",
+    "url": "/js/app.js"
+  },
+  {
+    "revision": "0166d3aafa5ad6550115",
+    "url": "/js/chunk.js"
+  },
+  {
+    "revision": "ae9b4dd6e872a99745fa83aefd7c4e63",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
